e09a696ca062bb7ef1a69a97475aafda06ebfcb1
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2021-11-06 16:45:00 +0800

    use nested directories for cache
    (don't rly know why, but it's std practice)

--- lib/cacheControls.ts
+++ lib/cacheControls.ts
@@ -4,44 +4,58 @@ import { getCacheDir } from './cache.ts';
 
 const LIMIT = 0.5 * 1000 * 1000 * 1000; // 0.5 GB
 
-export const getInfo = async (dir: string) => {
+type FileData = (Deno.DirEntry & {
+  path?: string;
+  size?: number;
+  removalIndex?: number;
+});
+
+export const dfsInfo = async (dir: string) => {
+  const asyncIterFiles = Deno.readDir(dir);
   let totalSize = 0;
 
-  const asyncIterFiles = Deno.readDir(dir);
-  const files:
-    ({ name: string; size: number; path: string; removalIndex: number })[] = [];
+  let files: FileData[] = [];
 
   for await (const x of asyncIterFiles) {
-    const path = join(dir, x.name);
+    if (x.isFile) {
+      const path = join(dir, x.name);
 
-    const stats = await Deno.stat(path);
-    const size = stats.size;
-    const staleness = Date.now() - (stats.birthtime?.getTime() ?? Infinity);
+      const stats = await Deno.stat(path);
+      const size = stats.size;
+      const staleness = Date.now() - (stats.birthtime?.getTime() ?? Infinity);
 
-    totalSize += size;
-    files.push({ name: x.name, path, size, removalIndex: size * staleness });
+      totalSize = totalSize + size;
+      files = [...files, {
+        ...x,
+        path,
+        size,
+        removalIndex: size * staleness,
+      }];
+    } else if (x.isDirectory) {
+      const deeperInfo = await dfsInfo(join(dir, x.name));
+
+      files = [...files, ...deeperInfo.files];
+      totalSize += deeperInfo.size;
+    } else {
+      throw new Error('symlink found!');
+    }
   }
 
-  debug(`Cache size ${(totalSize / 1000 / 1000).toFixed(2)}MB`);
-
-  return {
-    size: totalSize,
-    files: files.sort((a, b) => a.removalIndex < b.removalIndex ? 1 : -1),
-  };
+  return { size: totalSize, files };
 };
 
 export const prune = async () => {
   const dir = await getCacheDir();
 
-  let { size, files } = await getInfo(dir);
+  let { size, files } = await dfsInfo(dir);
 
   if (size <= LIMIT) return -1;
 
   let i = 0;
 
   while (size > LIMIT) {
-    await Deno.remove(files[i].path);
-    size -= files[i].size;
+    await Deno.remove(files[i].path!);
+    size -= files[i].size ?? 0;
 
     i++;
     debug(
--- tests/test.ts
+++ tests/test.ts
@@ -40,7 +40,7 @@ Deno.test('Prune', async () => {
 
   await cacheAction('prune');
 
-  const { size } = await (await import('./../lib/cacheControls.ts')).getInfo(
+  const { size } = await (await import('./../lib/cacheControls.ts')).dfsInfo(
     cacheDir,
   );
 
