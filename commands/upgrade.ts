e7cc7229ed5dc9349f45e5070854beefec17c5ab
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2021-12-12 00:57:25 +0800

    fix: extra `v` in upgrade url

--- commands/upgrade.ts
+++ commands/upgrade.ts
@@ -72,8 +72,8 @@ const denoUpgrade = async () => {
       '--name',
       'clank',
       '--import-map',
-      `https://deno.land/x/clank@v${latest}/import_map.json`,
-      `https://deno.land/x/clank@v${latest}/mod.ts`,
+      `https://deno.land/x/clank@${latest}/import_map.json`,
+      `https://deno.land/x/clank@${latest}/mod.ts`,
     ],
   }).status();
 
