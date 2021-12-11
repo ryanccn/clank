650ad3a7c680cf6edd57eeadf2fa6a18773010ea
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2021-12-12 00:09:15 +0800

    chore(vr): use ts config file

---
+++ scripts.ts
@@ -0,0 +1,22 @@
+import type { ScriptsConfiguration } from 'https://deno.land/x/velociraptor@1.3.0/mod.ts';
+
+export default <ScriptsConfiguration> {
+  imap: './import_map.json',
+
+  scripts: {
+    build: {
+      cmd: '_build.ts',
+      allow: ['run', 'read', 'write'],
+      desc: 'Build native binaries',
+    },
+    'install-dev': {
+      cmd: 'deno install -Af -n "clank-dev" mod.ts',
+      desc: 'Install your local version of clank as `clank-dev`',
+    },
+    test: {
+      cmd: 'deno test',
+      allow: 'all',
+      desc: 'Run tests',
+    },
+  },
+};
