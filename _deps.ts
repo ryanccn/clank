e82642bc777f7cdff4114a26350afb7d6eabd2fe
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-01-31 12:34:34 +0800

    chore(deps): bump deps

--- _deps.ts
+++ _deps.ts
@@ -5,9 +5,9 @@ export {
 } from 'https://deno.land/x/cliffy@v0.20.1/command/mod.ts';
 export { Table } from 'https://deno.land/x/cliffy@v0.20.1/table/mod.ts';
 
-export { Sha256 } from 'https://deno.land/std@0.118.0/hash/sha256.ts';
+export { Sha256 } from 'https://deno.land/std@0.123.0/hash/sha256.ts';
 
-export { writeAll } from 'https://deno.land/std@0.118.0/streams/conversion.ts';
-export { join } from 'https://deno.land/std@0.118.0/path/mod.ts';
+export { writeAll } from 'https://deno.land/std@0.123.0/streams/conversion.ts';
+export { join } from 'https://deno.land/std@0.123.0/path/mod.ts';
 
-export * from 'https://deno.land/std@0.118.0/fmt/colors.ts';
+export * from 'https://deno.land/std@0.123.0/fmt/colors.ts';
